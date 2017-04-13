15b1da89d234be74d442ca9f41ed2e388f3903fb
Himmelt <master@void-3.cn>
2017-04-13 19:48:24 +0800

    first commit

---
+++ src/include/tag.h
@@ -0,0 +1,23 @@
+#ifndef LIBNBT_TAG_H
+#define LIBNBT_TAG_H
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <typeinfo>
+
+namespace libnbt {
+
+    using namespace std;
+
+    class NBTBase {
+    private:
+
+    public:
+    };
+}
+
+#endif //LIBNBT_TAG_H
